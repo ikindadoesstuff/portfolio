0d145331607e01853b92569aef0ddcffddb82740
Devyon Lozano <devyonlozano@gmail.com>
2025-06-17 01:50:19 -0600

    chore: add routing and global styles

---
+++ src/Landing.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './Landing.css'
+
+function Landing() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default Landing
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { BrowserRouter, Routes, Route } from 'react-router'
+
+// Import Pages
+import Landing from './Landing.tsx'
+
+// Import Stylesheet
+import './global.css'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+	<StrictMode>
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Landing/>}>
+
+				</Route>
+			</Routes>
+		</BrowserRouter>
+	</StrictMode>,
 )
